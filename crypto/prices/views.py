a64806a9c5119fffae49afc60d6857d094eaeddd
kbnaveen123 <kbnaveen123@gmail.com>
2021-06-22 22:17:58 +0530

    index page created

--- crypto/prices/views.py
+++ crypto/prices/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+import requests
+
+key = "7841e24796f05e44b6956c2786d96e0828cf3e97"
 
 # Create your views here.
+def index(request):
+    url = "https://api.nomics.com/v1/currencies/ticker?key="+key+"&interval=1h,1d,7d&convert=USD&per-page=15"
+    res = requests.get(url).json()
+    return render(request, 'index.html', {'res':res})
